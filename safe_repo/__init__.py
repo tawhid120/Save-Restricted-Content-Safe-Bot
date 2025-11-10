8bb4c7c6c53317ad441b09ff02f641110cb91a8b
tawhid120 <lorddanju@gmail.com>
2025-11-10 13:42:34 +0600

    Update __init__.py

--- safe_repo/__init__.py
+++ safe_repo/__init__.py
@@ -3,7 +3,8 @@
 
 import asyncio
 import logging
-from pyromod import Client, listen
+from pyromod import listen
+from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 from telethon.sync import TelegramClient
 
